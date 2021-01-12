dbdcbe23380950aa1c7f9886e8feb0a4c7a6409c
Lita Beach <litabeach123@gmail.com>
2021-01-11 22:45:59 -0600

    buttons work

--- Assets/ReadMe.md
+++ Assets/ReadMe.md
@@ -1,2 +1,4 @@
 Resources used:
 https://www.sitepoint.com/simple-javascript-quiz/
+
+https://stackoverflow.com/questions/11969412/how-do-i-make-a-loop-advance-using-onclick
--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,17 @@
 // create variables
 
-numRight = 0,
-numQuestions = 0,
-answerDiv = document.getElementById('answers'),
-questionDiv = document.getElementById('question'),
-numRightSpan = document.getElementById('numRight'),
+numRight = 0;
+numQuestions = 0;
+answerDiv = document.getElementById('answers');
+questionDiv = document.getElementById('question');
+numRightSpan = document.getElementById('numRight');
 numQuestionsSpan = document.getElementById('numQuestions');
 startQuiz = document.getElementById('start-button');
 questions = [
   {
       question: 'What is 1 + 1 ?',
-      options: ['0','1','2'],
-      correctIndex: 0
+      options: ['0','2','4'],
+      correctIndex: 1
   },
   {
       question: 'What is 2 + 2 ?',
@@ -29,19 +29,25 @@ questions = [
     options: ['5','8','14'],
     correctIndex: 2
   },
-]
+];
+
+// When you click start quiz, timer begins and questions appear
+startQuiz.addEventListener("click", function(event){
+  event.preventDefault();
+  setTime();
+  displayQuestion(questions.shift());
+});
 
 
-
-function showQuestion(q) {  
-  // insert the question text
+function displayQuestion(q) {  
+  // question text in HTML
   questionDiv.innerHTML = q.question;
 
-  // clear existing 
+  // clear answer buttons
   answerDiv.innerHTML = '';
 
   // for each option in the 'options' array, create a button
-  // attach an 'onclick' event handler that will update
+  // attach an 'onclick' event that will update
   // the question counts and display the next question in the array
   for(i = 0; i < q.options.length; i++) {
       btn = document.createElement('button');
@@ -53,13 +59,16 @@ function showQuestion(q) {
           var id = parseInt(this.getAttribute('id'),10);
           numQuestionsSpan.innerHTML = ++numQuestions;
 
-          // if this is the right answer, increment numRight
+          // if this is the right answer, increment numRight, if not subtract 10 seconds from timer
           if(id === q.correctIndex) {
               numRightSpan.innerHTML = ++numRight;
           }
-
+          else {
+            secondsLeft -= 10;
+          }
+        
           // if there is another question to be asked, run the function again
-          // otherwise, complete the test however you see fit
+          // otherwise, complete the test
           if(questions.length) {
               displayQuestion(questions.shift()); 
           } else {
@@ -70,39 +79,22 @@ function showQuestion(q) {
   }
 }
 
-startQuiz.addEventListener("click", function(event){
-  event.preventDefault();
-  setTime();
-});
-
-  showQuestion(questions.shift());
-
   // timer
 
   var timerEl = document.querySelector(".timer");
   
-  var secondsLeft = 90;
+  var secondsLeft = 30;
   
   function setTime() {
     var timerInterval = setInterval(function() {
       secondsLeft--;
       timerEl.textContent = secondsLeft + " seconds left.";
       
-      if(secondsLeft === 0) {
+      if(secondsLeft <= 0) {
         clearInterval(timerInterval);
-        // showResults();
+        alert('Done! You got '+numRight+' of '+ 4 +' right!');
       }
   
     }, 1000);
   }
-
-  // function incorrect() {
-  //   timerEl.textContent = secondsLeft + " seconds left.";
-  //   secondsLeft -= 10;
-  //   if(secondsLeft <= 0) {
-  //     clearInterval(timerInterval);
-      
-  //   }
-  // }
-
 
